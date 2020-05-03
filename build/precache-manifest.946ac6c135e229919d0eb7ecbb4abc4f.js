da9164d45360ad9e1330056d3aa45f8224bbbdf6
CptJackFrost <cptjackfrost74@gmail.com>
2020-05-03 19:13:45 +0500

    билд

---
+++ build/precache-manifest.946ac6c135e229919d0eb7ecbb4abc4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ae91edca4bc28fafc6f813739fd197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf4d07008ddc3765b613",
+    "url": "/static/css/main.385bc1e9.chunk.css"
+  },
+  {
+    "revision": "02f8b277998eb6de184b",
+    "url": "/static/js/2.8ed07ff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ed07ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf4d07008ddc3765b613",
+    "url": "/static/js/main.b10a8d29.chunk.js"
+  },
+  {
+    "revision": "42ef68a2d5e45464b8c1",
+    "url": "/static/js/runtime-main.3ccbe257.js"
+  }
+]);
